667ff9595732b7b6f77ca732de830baa35195d62
randor12 <rynicholas@vt.edu>
2019-05-11 15:20:35 -0400

    edited auto save

--- AutoSave.cpp
+++ AutoSave.cpp
@@ -5,12 +5,58 @@
 
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 #include <string>
 #include <vector>
+#include <filesystem>
+#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <corecrt_wstring.h>
 
 using namespace std;
-namespace fs = std::filesystem;
+namespace fs = std::experimental::filesystem;
+
+/**
+ * Following code was found on stack overflow
+ */
+vector<wstring> get_all_files_names_within_folder(wstring folder)
+{
+	vector<wstring> names = {};
+	wstring search_path = folder;
+	WIN32_FIND_DATA fd;
+	HANDLE hFind = ::FindFirstFile(search_path.c_str(), &fd);
+	if (hFind != INVALID_HANDLE_VALUE) {
+		do {
+			// read all (real) files in current folder
+			// , delete '!' read other 2 default folder . and ..
+			if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+				names.push_back(fd.cFileName);
+			}
+		} while (::FindNextFile(hFind, &fd));
+		::FindClose(hFind);
+	}
+	return names;
+}
+
+/**
+ * Convert a string into a wstring
+ */
+wstring toWstring(string word)
+{
+	wstring temp(word.begin(), word.end());
+
+	return temp;
+}
+
+/**
+ * converts a wstring to a string
+ */
+string toString(wstring word)
+{
+	string temp(word.begin(), word.end());
+
+	return temp;
+}
 
 /**
  * This is the main class
@@ -19,15 +65,20 @@ namespace fs = std::filesystem;
 int main()
 {
 	string path;
-	vector<string> files;
+	//vector<int> files = {};
 	cout << "Input the path: " << endl;
 	cin >> path;
 
-	for (const auto& entry : fs::directory_iterator(path))
-	{
-		files.push_back(entry.path());
+	wstring folder = toWstring(path);
+
+	vector<wstring> files = get_all_files_names_within_folder(folder);
+
+	for (wstring getFile : files) {
+		string actualFile = toString(getFile);
+		cout << actualFile << endl;
 	}
 
+	/*
 	for (string getFile : files) {
 		if (!getFile._Equal(".git")) {
 			ofstream outfile;
@@ -35,6 +86,7 @@ int main()
 			outfile.close();
 		}
 	}
+	*/
 
 	return 0;
 }
