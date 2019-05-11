fc686571cbd53b6e63fee0c6b2a46c32929ac8b0
randor12 <rynicholas@vt.edu>
2019-05-10 23:17:35 -0400

    changed code

--- GitPushPull.py
+++ GitPushPull.py
@@ -14,8 +14,12 @@ while True:
     try:
         if keyboard.is_pressed("ctrl") and keyboard.is_pressed("s"):
             fileList = os.listdir(path)
+            actualFiles = []
+            for file in fileList:
+                if not file.__contains__(".git") and not file.__contains__("README"):
+                    actualFiles.append(file)
             commitMessage = "Changed code"
-            repo.index.add(fileList)
+            repo.index.add(actualFiles)
             repo.index.commit(commitMessage)
             origin = repo.remote('origin')
             origin.push()
